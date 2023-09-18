76039e097fafc81b650ed41ce3c2cda315944206
azharhussain78 <azharhussainshahani78@gmail.com>
2023-09-18 09:14:44 +0500

    Update A.java

--- A.java
+++ A.java
@@ -1,6 +1,9 @@
+import java.util.*;
 class A{
 public static void main(String args[]){
 	System.out.println("WELCOME");
-	System.out.println("I AM RUNNING WITH FAST AND YOU WHERE DEAR!@");
+	Scanner scanner = new Scanner(System.in);
+	int a = scanner.nextInt();
+	System.out.println(a);
 	}
 }	
